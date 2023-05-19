12f59b40f1c4e00673d6331d3d841c033c28e2c4
Mashwishi <Mashwishiii@gmail.com>
2023-05-19 17:51:19 +0800

    4.1.5

--- helper/messengerApi.js
+++ helper/messengerApi.js
@@ -4,29 +4,54 @@ require('dotenv').config();
 const TOKEN = process.env.TOKEN;
 const PAGE_ID = process.env.PAGE_ID;
 
-const sendMessage = async(senderId, message) => {
-
-  //v11.0
-    let options = {
-      method: 'POST',
-      url: `https://graph.facebook.com/v11.0/${PAGE_ID}/messages`,
-      params: {
-        access_token: TOKEN,
-        recipient: JSON.stringify({'id': senderId}),
-        messaging_type: 'RESPONSE',
-        message: JSON.stringify({'text': message})
-      }
-    };
-
-    let response = await axios.request(options);
-
-    if (response['status'] == 200 && response['statusText'] === 'OK') {
-        return 1;
-    } else {
-        return 0;
+const sendTypingIndicator = async (senderId, senderAction) => {
+  let options = {
+    method: 'POST',
+    url: `https://graph.facebook.com/v11.0/${PAGE_ID}/messages`,
+    params: {
+      access_token: TOKEN,
+    },
+    data: {
+      recipient: {
+        id: senderId
+      },
+      sender_action: senderAction
     }
+  };
+
+  await axios.request(options);
+};
+
+const sendMessage = async (senderId, message) => {
+  await sendTypingIndicator(senderId, 'typing_on');
+
+  let options = {
+    method: 'POST',
+    url: `https://graph.facebook.com/v11.0/${PAGE_ID}/messages`,
+    params: {
+      access_token: TOKEN,
+    },
+    data: {
+      recipient: {
+        id: senderId
+      },
+      messaging_type: 'RESPONSE',
+      message: {
+        text: message
+      }
+    }
+  };
+
+  let response = await axios.request(options);
+
+  if (response['status'] == 200 && response['statusText'] === 'OK') {
+    await sendTypingIndicator(senderId, 'typing_off');
+    return 1;
+  } else {
+    return 0;
+  }
 };
 
 module.exports = {
-    sendMessage
-}
+  sendMessage
+};
